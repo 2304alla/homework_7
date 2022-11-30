30cfa2c9703d30bd003f4b063730e9a5d54ef3fe
allakoval <2448572@gmail.com>
2022-11-30 20:02:35 +0500

    task47/50/52

---
+++ task50/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 50.
+//  Напишите программу, которая на вход принимает позиции элемента в двумерном массиве
+//  , и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1, 7 -> такого элемента в массиве нет
+
+int[,] CreateMatrixSumIndex(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}| ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+bool SearhCoordinate(int[,] matrix, int m, int n)
+{
+    if (m < matrix.GetLength(0) && n < matrix.GetLength(1)) return true;
+    return false;
+}
+
+Console.WriteLine("Введите координаты");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] arraY2D = CreateMatrixSumIndex(3, 4);
+PrintMatrix(arraY2D);
+Console.WriteLine();
+bool result = SearhCoordinate(arraY2D, a, b);
+Console.WriteLine(result ? $"Значение элемента: {arraY2D[a, b]}" : "такого элемента в массиве нет");
+
+
---
+++ task52/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 52. 
+// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.;
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}; ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+double[] MeanSumColumns(int[,] matrix)
+{
+    double[] array = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+
+        }
+        double sa = sum / matrix.GetLength(0);
+        array[j] = Math.Round(sa, 1);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}; ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+
+
+int[,] arraY2D = CreateMatrix(3, 4, 0, 9);
+PrintMatrix(arraY2D);
+Console.WriteLine();
+
+double[] arrayRes = MeanSumColumns(arraY2D);
+
+
+Console.WriteLine("Среднее арифметическое каждого столбца");
+PrintArray(arrayRes);
